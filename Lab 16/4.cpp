c5d613b9ee9be947bb8c2527dfe4377e151b1376
TaiModay1010 <153613325+TaiModay1010@users.noreply.github.com>
2024-01-05 13:48:16 +0700

    Update 4.cpp

--- Lab 16/4.cpp
+++ Lab 16/4.cpp
@@ -8,7 +8,7 @@ typedef struct Sinhvien{
 
 int main(){
 	Sinhvien sv[20];
-	for(int i=0;i<2;i++){
+	for(int i=0;i<20;i++){
 		printf("Nhap ten sinh vien:");
 		scanf("%s",sv[i].name);
 		printf("diem: ");
@@ -16,19 +16,21 @@ int main(){
 	}
 	
 	for(int i=0;i<20-1;i++){
-		for(int j=0;j<2-i-1;j++){
-			sv temp;
+		for(int j=0;j<20-i-1;j++){
+			char temp[20];
 			if(strcmp(sv[j].name,sv[j+1].name)>0){
-				strcpy(temp.name,sv[j].name);
-				temp.mark = sv[j].mark;
+				strcpy(temp,sv[j].name);
 				strcpy(sv[j].name,sv[j+1].name);
-				strcpy(sv[j+1].name,temp.name);
+				strcpy(sv[j+1].name,temp);
+				temp[20] = sv[j].mark;
+				sv[j].mark = sv[j+1].mark;
+				sv[j+1].mark = temp[20];
 				
 			}
 		}
 	}
 	
-	for(int i=0;i<2;i++){
+	for(int i=0;i<20;i++){
 		printf("%s %d\n",sv[i].name,sv[i].mark);
 	}
 	
